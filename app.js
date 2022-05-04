8ce550949806d5c942b5b6b8cde865fa7a4cffcf
Andrés García <playsharp59@gmail.com>
2022-05-04 12:51:59 +0200

    added cors

--- app.js
+++ app.js
@@ -13,4 +13,10 @@ app.use("/api", allRoutes)
 
 require("./error-handling")(app)
 
+app.all('/', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+});
+
 module.exports = app
